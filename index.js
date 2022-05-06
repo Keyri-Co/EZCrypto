67427b24313c1b8388e7374aeddd8521b4a0eee7
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2022-05-06 12:58:04 -0500

    add methods to encrypt / decrypt with HKDF

--- index.js
+++ index.js
@@ -394,6 +394,226 @@ export class EZCrypto {
     );
   };
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // //////////////////////////////////////////////////////////////////////////
+  // //////////////////////////////////////////////////////////////////////////
+  //
+  // Function:     HKDFEncrypt (async)
+  // What is this: Encrypt Uint8Data with 2 SPKI-Encoded ECDH Keys.
+  //               ---
+  //               Basically it does the dirty work of:
+  //               - convert base64 keys to live keys
+  //               - creating AES key from live keys
+  //               - encrypting data with AES Key
+  //               - return base64 ciphertext and nonce
+  //
+  //
+  // Arguments:    base64privateKey: string;
+  //               base64publicKey: string;
+  //               base64data: string;
+  //
+  // Returns:      object containing public and private key pair
+  // Notes:
+  //
+  // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  HKDFEncrypt = async (b64Private, b64Public, b64data) => {
+
+    // 1.) convert the given keys to real keys in the most
+    //     generic way possible...
+    let publicKey = await this.EcConvertKey(b64Public);
+    let privateKey = await this.EcConvertKey(b64Private);
+    
+    // 2.) generate shared secret for HKDF
+    //
+    let sharedSecret = await window.crypto.subtle.deriveBits({ 
+      "name": "ECDH", 
+      "namedCurve": "P-256", 
+      "public": publicKey 
+    },privateKey,256);
+    
+    // 3.) convert shared-secret into a key
+    //
+    let sharedSecretKey = await window.crypto.subtle.importKey(
+      "raw", sharedSecret, 
+      { "name": 'HKDF' }, 
+      false, 
+      ['deriveKey','deriveBits']
+    );
+    
+    // 4.) create SALT
+    //
+    let salt = window.crypto.getRandomValues(new Uint8Array(16));
+    
+    // 5.) convert the live-shared-secret-key into an aes key
+    //
+    let derivedKey = await window.crypto.subtle.deriveBits({
+      "name": 'HKDF', 
+      "hash": 'SHA-256', 
+      "salt": salt,
+      "info": new Uint8Array([])},
+      sharedSecretKey,256
+    );
+    
+    //
+    // 6.) 
+    //   ___  __  __ _____ ____
+    //  / _ \|  \/  |  ___/ ___|
+    // | | | | |\/| | |_ | |  _
+    // | |_| | |  | |  _|| |_| |
+    //  \___/|_|  |_|_|   \____|
+    //
+    // THIS SHOULD NOT BE THIS HARD!
+    //
+    //     Convert the Key-Array to a live Key
+    let aes_key = await window.crypto.subtle.importKey(
+      "raw",
+      derivedKey,
+      "AES-GCM",
+      true,
+      ["encrypt","decrypt"]
+    );
+    
+    // 7.) Encrypt
+    //
+    //
+    let encrypted = await window.crypto.subtle.encrypt(
+      { name: "AES-GCM", iv: salt },
+      aes_key,
+      this.base64ToArray(b64data)
+    );
+    
+    // 8.) Base64 and return our data...
+    return {
+      "ciphertext": this.arrayToBase64(new Uint8Array(encrypted)),
+      "salt": this.arrayToBase64(salt)
+    };
+
+  
+  };
+
+  // //////////////////////////////////////////////////////////////////////////
+  // //////////////////////////////////////////////////////////////////////////
+  //
+  // Function:     HKDFDecrypt (async)
+  // What is this: Decrypt Uint8Data with 2 SPKI-Encoded ECDH Keys.
+  //               ---
+  //               Basically it does the dirty work of:
+  //               - convert base64 keys to live keys
+  //               - creating AES key from live keys
+  //               - encrypting data with AES Key
+  //               - return base64 ciphertext and nonce
+  //
+  //
+  // Arguments:    base64privateKey: string;
+  //               base64publicKey: string;
+  //               base64nonce: string;
+  //               base64data: string;
+  //
+  // Returns:      object containing public and private key pair
+  // Notes:
+  //
+  // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  HKDFDecrypt = async (b64Private, b64Public, b64Salt, b64data) => {
+
+    // 1.) convert the given keys to real keys in the most
+    //     generic way possible...
+    let publicKey = await this.EcConvertKey(b64Public);
+    let privateKey = await this.EcConvertKey(b64Private);
+    let salt = this.base64ToArray(b64Salt);
+    let data = this.base64ToArray(b64data);
+    
+    
+    // 2.) generate shared secret for HKDF
+    //
+    let sharedSecret = await window.crypto.subtle.deriveBits({ 
+      "name": "ECDH", 
+      "namedCurve": "P-256", 
+      "public": publicKey 
+    },privateKey,256);
+    
+    // 3.) convert shared-secret into a key
+    //
+    let sharedSecretKey = await window.crypto.subtle.importKey(
+      "raw", sharedSecret, 
+      { "name": 'HKDF' }, 
+      false, 
+      ['deriveKey','deriveBits']
+    );
+    
+    
+    // 4.) convert the live-shared-secret-key into an aes key
+    //
+    let derivedKey = await window.crypto.subtle.deriveBits({
+      "name": 'HKDF', 
+      "hash": 'SHA-256', 
+      "salt": salt,
+      "info": new Uint8Array([])},
+      sharedSecretKey,256
+    );
+    
+    //
+    // 5.) 
+    //   ___  __  __ _____ ____
+    //  / _ \|  \/  |  ___/ ___|
+    // | | | | |\/| | |_ | |  _
+    // | |_| | |  | |  _|| |_| |
+    //  \___/|_|  |_|_|   \____|
+    //
+    // THIS SHOULD NOT BE THIS HARD!
+    //
+    //     Convert the Key-Array to a live Key
+    let aes_key = await window.crypto.subtle.importKey(
+      "raw",
+      derivedKey,
+      "AES-GCM",
+      true,
+      ["encrypt","decrypt"]
+    );
+    
+
+
+    // 6.) decrypt our data
+    //
+    return await window.crypto.subtle.decrypt(
+      { name: "AES-GCM", iv: salt },
+      aes_key,
+      data
+    );
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
@@ -569,7 +789,7 @@ export class EZCrypto {
             this.base64ToArray(base64key),
             { name: "ECDH", namedCurve: "P-256" },
             true,
-            ["deriveKey"]
+            ["deriveKey","deriveBits"]
           );
             
           return key;
